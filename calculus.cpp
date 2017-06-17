d7e9b685cf8950a1b633093ebc0ff6a3a0a4dde7
rickydia <rickydia@umich.edu>
2017-06-17 00:52:38 -0700

    Added more info to .cpp

--- calculus.cpp
+++ calculus.cpp
@@ -4,7 +4,6 @@
  * rickydia@umich.edu
  *
  * Started on the 12th of June, 2017 at 12:50 PM
- * Last edited on the 13th of June, 2017 at 1:22 PM
  *
  */
 
@@ -16,72 +15,54 @@
 #include <cassert>
 
 
+// Function for the output of a dericative
+void output_for_der(int num_of_der, std::string der, std::string output);
 
-//int main(int argc, char* argv[]){
-	//if(argc == 1){
-		//std::cout << "CHECK USAGE FOR SPECIFIC TYPE BY TYPING: ./calculus [d - derivative, i - integral, s - sum]" << std::endl;
-		//std::cout << "PROGRAM TERMINATED." << std::endl;
-	//}
-
-	//if(std::string(argv[1]) == "d"){
-		//if(argc != 4){
-			//std::cout << "USAGE FOR DERIVATIVES: ./calculus d [NUMBER OF TIMES THE DERIVATIVE WILL BE TAKEN] [OBJECT OF DERIVATION]" << std::endl;
-			//std::cout << "PROGRAM TERMINIATED." << std::endl;
-		//}
-		
-		//Derivative derivative( atoi(argv[2]), argv[3] );
-		
-		//derivative.take_derivative();
-	//}
-
-	//if(std::string(argv[1]) == "i"){
-		//if(argc != 3 && argc != 5){
-			//std::cout << "USAGE FOR INTERGALS: ./calculus i (POSSIBLE BOTTOM LIMIT OF INTEGRATION) (POSSIBLE TOP LIMIT OF INTEGRATION) [OBJECT OF INTEGRATION]" << std::endl;
-			//std::cout << "PROGRAM NOT YET IMPLEMENTED." << std::endl;
-			//std::cout << "PROGRAM TERMINATED." << std::endl;
-		//}
-		
-	//}
-
-	//if(std::string(argv[1]) == "s"){
-		//if(argc != 5){
-			//std::cout << "USAGE FOR SUMS: ./calculus s [BOTTOM LIMIT OF SUM] [TOP LIMIT OF SUM] [OBJECT OF SUMMATION]" << std::endl;
-			//std::cout << "PROGRAM NOT YET IMPLEMENTED." << std::endl;
-			//std::cout << "PROGRAM TERMINATED." << std::endl;
-		//}
-		
-	//}
-	
-//}
-
-void output_for_der(int num_of_der, std::string der);
 
 int main(){
+	// Runs until user enters "q" or "quit"
 	while(true){
-		std::cout << "Please enter what you would like to do: \n" << std::endl;
-		std::cout << "d - derivative" << std::endl;
-		std::cout << "i - integral" << std::endl;
-		std::cout << "s - sum" << std::endl;
-		std::cout << "q - quit \n" << std::endl;
+		// Prompts user for action
 		std::string input;
+		std::cout << "Please enter what you would like to do: \n" << std::endl;
+		std::cout << "         d - derivative" << std::endl;
+		std::cout << "         i - integral" << std::endl;
+		std::cout << "         q - quit \n" << std::endl;
 		std::cin >> input;
 		
+		// Breaks out of loop if user wants to quit
 		if(input == "q" || input == "quit"){
 			break;
 		}
 
+		// If user wants to take the derivative
 		else if(input == "d" || input == "derivative"){
-			std::cout << "Please enter the object of derivation: ";
 			std::string der;
+			std::cout << "Please enter the object of derivation: ";
 			std::cin >> der;
 			
-			std::cout << "Please enter the amount of times the derivative should be taken: ";
 			int num_of_der = 0;
+			std::cout << "Please enter the amount of times the derivative should be taken: ";
 			std::cin >> num_of_der;
 
+			// Creates a derivative object
 			Derivative derivative(num_of_der, der);
 			std::string output;
+			// Takes a derivative and stores it to "object"
 			output = derivative.take_derivative();
+			// Outputs the derivative to the user
+			output_for_der(num_of_der, der, output);
+		}
+
+		// If user wants to take the integral
+		else if(input == "i" || input == "integral"){
+			std::string inte;
+			std::cout << "Please enter the object of integration: ";
+			std::cin >> inte;
+
+			int num_of_inte = 0;
+			std::cout << "Please enter the amount of times the integral should be taken: ";
+			std::cin >> num_of_inte;
 
 		}
 			
@@ -90,8 +71,33 @@ int main(){
 	return 0;
 }
 
+
+
+// Function for the output of a dericative, mostly designed to get the correct suffix
 void output_for_der(int num_of_der, std::string der, std::string output){
-	if(num_of_der == 1){
+	std::cout << std::endl;
+
+	// After 20, the last digit determins the suffix
+	// NOTE: DOES NOT ACCOUNT FOR CORRECT SUFFIXES AFTER 110
+	if(num_of_der > 20){
+		if(num_of_der % 10 == 1){
+			std::cout << "The " << num_of_der << "st derivative of " << der << " is " << output << "." << std::endl;
+		}
+
+		else if(num_of_der % 10 == 2){
+			std::cout << "The " << num_of_der << "nd derivative of " << der << " is " << output << "." << std::endl;
+		}
+
+		else if(num_of_der % 10 == 3){
+			std::cout << "The " << num_of_der << "rd derivative of " << der << " is " << output << "." << std::endl;
+		}
+
+		else{
+			std::cout << "The " << num_of_der << "th derivative of " << der << " is " << output << "." << std::endl;
+		}
+	}
+
+	else if(num_of_der == 1){
 		std::cout << "The 1st derivative of " << der << " is " << output << "." << std::endl;
 	}
 
@@ -103,29 +109,12 @@ void output_for_der(int num_of_der, std::string der, std::string output){
 		std::cout << "The 3rd derivative of " << der << " is " << output << "." << std::endl;
 	}
 
-	else if(num_of_der >= 21){
-		if(true){
-			std::cout << "The " << num_of_der << "st derivative of " << der << " is " << output << "." << std::endl;
-		}
-
-		else if(true){
-			std::cout << "The " << num_of_der << "nd derivative of " << der << " is " << output << "." << std::endl;
-		}
-
-		else if(true){
-			std::cout << "The " << num_of_der << "rd derivative of " << der << " is " << output << "." << std::endl;
-		}
-
-		else{
-			std::cout << "The " << num_of_der << "th derivative of " << der << " is " << output << "." << std::endl;
-		}
-	}
-
 	else{
 		std::cout << "The " << num_of_der << "th derivative of " << der << " is " << output << "." << std::endl;
 	}
 			
-			
+	std::cout << std::endl;
+	std::cout << std::endl;
 }
 			
 
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+	int number = 862;
+	std::cout << number % 10 << std::endl;
+}
